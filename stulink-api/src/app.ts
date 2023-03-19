fea784b28590aab64f280415f8e0e6a7331181a1
a-ww2-er <warhistories101@gmail.com>
2023-03-19 08:47:48 +0100

    further setup and structuring

--- stulink-api/src/app.ts
+++ stulink-api/src/app.ts
@@ -1,10 +1,37 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+//import routes
+import dashboardRoute from "./routes/dashboardRoute";
+
+//configurations
+dotenv.config();
+const port = process.env.PORT;
 const app: express.Application = express();
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.send("hello world ssss");
-});
+//middlewares
+app.use(express.json())
 
-app.listen(5000, () => {
-  console.log("server running on port 5000");
+//connect MongoDB
+const connectdb = async () => {
+  try {
+    // await mongoose.connect(process.env.MONGO); 
+    console.log(`Connected to MongoDB`);
+    
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//routes
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("hello world");
+});
+app.use("/dashboard", dashboardRoute);
+
+//port
+app.listen(port, () => {
+  connectdb()
+  console.log(`server running on port ${port}`);
 });

35ed36f255bbb8e338df5a4bf46a8ef6b0f3a006
a-ww2-er <warhistories101@gmail.com>
2023-06-25 21:12:24 +0300

    changed navbar position to sticky

--- stulink-api/src/middleware/authenticate.ts
+++ stulink-api/src/middleware/authenticate.ts
@@ -1,26 +1,29 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import { userInfo } from "os";
 import User from "../models/user.model";
 import { ErrorResponse } from "../utilities/errorResponse";
 
-export const protectRegularEndpoint = async (req,res,next) =>{
-   const token = req.cookies.accessToken
+export const protectRegularEndpoint = async (req, res, next) => {
+  const token = req.cookies.accessToken;
 
-    if(!token){
-        return next(new ErrorResponse("You are not authenticated",401))
+  if (!token) {
+    return next(new ErrorResponse("You are not authenticated", 401));
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return next(new ErrorResponse("User not found", 404));
     }
-    try {
-        const decoded = jwt.verify(token,process.env.JWT_SECRET);
-        const user = await User.findById(decoded.id)
-        if(!user){
-            return next(new ErrorResponse("User not found",404))
-        }
-      
-        next();
-    } catch (error) {
-        return next(new ErrorResponse("You are not authorized to access this page",401))
-    }
-}
+    //this will let us use the user in any endpoint
+    req.user = user;
+    next();
+  } catch (error) {
+    return next(
+      new ErrorResponse("You are not authorized to access this page", 401)
+    );
+  }
+};
 
 // export const protectAdminEndpoint =(req,res,next)=>{
 //     const token = req.cookies.accessToken;
